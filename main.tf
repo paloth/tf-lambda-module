f2edbb9efcac97fa6b545af42922de19650fb41a
Paul LOUIS THERESE <paul.louis-therese@d2si.io>
2019-10-11 15:55:56 +0200

    Add cloudwatch resources

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
+data "archive_file" "packaging" {
+  type        = var.packaging_type
+  source_file = var.source_file
+  output_path = var.output_path
+}
+
 resource "aws_lambda_function" "function" {
   description = var.description
 
-  filename      = var.filename
+  filename      = data.archive_file.packaging.output_path
   function_name = var.function_name
   role          = var.lambda_role_arn
   handler       = var.handler
 
-  source_code_hash = filebase64sha256(var.filename)
+  source_code_hash = filebase64sha256(data.archive_file.packaging.output_path)
 
   runtime                        = var.runtime
   memory_size                    = var.lambda_memory_size
@@ -18,4 +24,29 @@ resource "aws_lambda_function" "function" {
       variables = environment.value.variables
     }
   }
+}
+
+resource "aws_cloudwatch_event_rule" "trigger_function" {
+  name          = var.event_name
+  description   = var.event_description
+  event_pattern = ""
+  is_enabled    = var.is_event_enabled
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_event" {
+  statement_id  = "AllowExecutionFromCloudWatchEvent"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.function.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.trigger_function.arn
+}
+
+resource "aws_cloudwatch_event_target" "cloudwatch_event_target_function" {
+  rule = aws_cloudwatch_event_rule.trigger_function.name
+  arn  = aws_lambda_function.function.arn
+}
+
+resource "aws_cloudwatch_log_group" "lambda_event_emr_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.function.function_name}"
+  retention_in_days = var.log_retention_days
 }
