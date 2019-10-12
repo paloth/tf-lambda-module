6901aa0c609e11d1b4b241500536e1dfe2ef705f
Paul LOUIS THERESE <paul.louis-therese@d2si.io>
2019-10-12 17:00:50 +0200

    outputs correction

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "function_name" {
-  value = aws_lambda_function.function.output.function_name
+  value = aws_lambda_function.function.function_name
 }
 
 output "function_arn" {
-  value = aws_lambda_function.function.output.arn
+  value = aws_lambda_function.function.arn
 }
