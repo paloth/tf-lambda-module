f35c68bf2850320f7b66030e9ac3499287300fe1
Paul LOUIS THERESE <paul.louis-therese@d2si.io>
2019-11-25 22:05:00 +0100

    Add log group outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "function_name" {
 output "function_arn" {
   value = aws_lambda_function.function.arn
 }
+
+output "function_log_group" {
+  value = aws_cloudwatch_log_group.function_log_group.name
+}
+
+output "function_log_group_arn" {
+  value = aws_cloudwatch_log_group.function_log_group.arn
+}
